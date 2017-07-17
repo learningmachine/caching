26cc67013feb119e1a08f50ccfca0c25556736d1
Derek Pittsinger <derekpitt@gmail.com>
2017-07-17 14:32:58 -0500

    localstorage fallback to memory when can't use ls

--- dist/local-storage-cache.js
+++ dist/local-storage-cache.js
@@ -10,6 +10,18 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var _helpers = require("./helpers");
 
+var _memoryCache = require("./memory-cache");
+
+var canUseLocalStorage = true;
+var fallback = null;
+try {
+    localStorage.setItem("__testing", "__testing");
+    localStorage.removeItem("__testing");
+} catch (err) {
+    canUseLocalStorage = false;
+    fallback = new _memoryCache.MemoryCache();
+}
+
 var LocalStorageCache = (function () {
     function LocalStorageCache() {
         _classCallCheck(this, LocalStorageCache);
@@ -18,12 +30,14 @@ var LocalStorageCache = (function () {
     _createClass(LocalStorageCache, [{
         key: "set",
         value: function set(key, value, durationMS) {
+            if (!canUseLocalStorage) return fallback.set(key, value, durationMS);
             var expire = durationMS ? Date.now() + durationMS : null;
             localStorage[key] = JSON.stringify({ value: value, expire: expire });
         }
     }, {
         key: "get",
         value: function get(key) {
+            if (!canUseLocalStorage) return fallback.get(key);
             var val = JSON.parse(localStorage[key] || "null");
             if (val) {
                 if (!(0, _helpers.isExpired)(val)) {
@@ -37,6 +51,7 @@ var LocalStorageCache = (function () {
     }, {
         key: "remove",
         value: function remove(key) {
+            if (!canUseLocalStorage) return fallback.remove(key);
             delete localStorage[key];
         }
     }]);
--- src/local-storage-cache.ts
+++ src/local-storage-cache.ts
@@ -1,14 +1,30 @@
 import { Cache } from "./cache";
 import { isExpired } from "./helpers";
 
+import { MemoryCache } from "./memory-cache";
+
+let canUseLocalStorage = true;
+let fallback: MemoryCache = null;
+
+try {
+  localStorage.setItem("__testing", "__testing");
+  localStorage.removeItem("__testing");
+} catch(err) {
+  canUseLocalStorage = false;
+  fallback = new MemoryCache();
+}
+
 export class LocalStorageCache implements Cache {
   public set<T>(key: string, value: T, durationMS?: number): void {
-    const expire = durationMS ? Date.now() + durationMS : null;
+    if (!canUseLocalStorage) return fallback.set<T>(key, value, durationMS);
 
+    const expire = durationMS ? Date.now() + durationMS : null;
     localStorage[key] = JSON.stringify({ value, expire });
   }
 
   public get<T>(key: string): T {
+    if (!canUseLocalStorage) return fallback.get<T>(key);
+
     const val = JSON.parse(localStorage[key] || "null");
 
     if (val) {
@@ -23,6 +39,8 @@ export class LocalStorageCache implements Cache {
   }
 
   public remove(key: string): void {
+    if (!canUseLocalStorage) return fallback.remove(key);
+
     delete localStorage[key];
   }
 }
