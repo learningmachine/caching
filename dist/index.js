572260ed48c10a79cabd9e63c8e5baba2d19d2b7
Derek Pittsinger <derekpitt@gmail.com>
2017-07-17 15:47:36 -0500

    switch to rollup build

--- dist/index.js
+++ dist/index.js
@@ -1,59 +1,217 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _cache = require("./cache");
-
-Object.defineProperty(exports, "Cache", {
-  enumerable: true,
-  get: function get() {
-    return _cache.Cache;
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
   }
-});
+};
 
-var _helpers = require("./helpers");
-
-Object.defineProperty(exports, "cacheFor", {
-  enumerable: true,
-  get: function get() {
-    return _helpers.cacheFor;
+var createClass = function () {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
   }
-});
 
-var _memoryCache = require("./memory-cache");
+  return function (Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+}();
 
-Object.defineProperty(exports, "MemoryCache", {
-  enumerable: true,
-  get: function get() {
-    return _memoryCache.MemoryCache;
-  }
-});
+var Cache = function Cache() {
+  classCallCheck(this, Cache);
+};
 
-var _cookieCache = require("./cookie-cache");
+function isExpired(val) {
+    if (val.expire == null) return false;
+    return val.expire < Date.now();
+}
+function cacheFor(n) {
+    var seconds = n * 1000;
+    var minutes = n * 60 * 1000;
+    var hours = n * 60 * 60 * 1000;
+    return { minutes: minutes, seconds: seconds, hours: hours };
+}
 
-Object.defineProperty(exports, "CookieCache", {
-  enumerable: true,
-  get: function get() {
-    return _cookieCache.CookieCache;
-  }
-});
+var MemoryCache = function () {
+    function MemoryCache() {
+        classCallCheck(this, MemoryCache);
 
-var _localStorageCache = require("./local-storage-cache");
+        this.cache = {};
+    }
 
-Object.defineProperty(exports, "LocalStorageCache", {
-  enumerable: true,
-  get: function get() {
-    return _localStorageCache.LocalStorageCache;
-  }
-});
+    createClass(MemoryCache, [{
+        key: "set",
+        value: function set$$1(key, value) {
+            var durationMS = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
 
-var _scopedCache = require("./scoped-cache");
+            var expire = durationMS ? Date.now() + durationMS : null;
+            this.cache[key] = { value: value, expire: expire };
+        }
+    }, {
+        key: "get",
+        value: function get$$1(key) {
+            var val = this.cache[key];
+            if (val) {
+                if (!isExpired(val)) {
+                    return val.value;
+                } else {
+                    this.remove(key);
+                }
+            }
+            return null;
+        }
+    }, {
+        key: "remove",
+        value: function remove(key) {
+            delete this.cache[key];
+        }
+    }, {
+        key: "clearAll",
+        value: function clearAll() {
+            this.cache = {};
+        }
+    }]);
+    return MemoryCache;
+}();
 
-Object.defineProperty(exports, "ScopedCache", {
-  enumerable: true,
-  get: function get() {
-    return _scopedCache.ScopedCache;
-  }
-});
+var CookieCache = function () {
+    function CookieCache() {
+        var CookieName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "__cookie_cache";
+        classCallCheck(this, CookieCache);
+
+        this.CookieName = CookieName;
+    }
+
+    createClass(CookieCache, [{
+        key: "set",
+        value: function set$$1(key, value) {
+            var durationMS = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
+            var expire = durationMS ? Date.now() + durationMS : null;
+            var currentEntries = this.getCacheEntries();
+            currentEntries[key] = { value: value, expire: expire };
+            this.setCacheEntries(currentEntries);
+        }
+    }, {
+        key: "get",
+        value: function get$$1(key) {
+            var currentEntries = this.getCacheEntries();
+            var val = currentEntries[key];
+            if (val) {
+                if (!isExpired(val)) {
+                    return val.value;
+                } else {
+                    // expired, delete
+                    this.remove(key);
+                }
+            }
+            return null;
+        }
+    }, {
+        key: "remove",
+        value: function remove(key) {
+            var currentEntries = this.getCacheEntries();
+            delete currentEntries[key];
+            this.setCacheEntries(currentEntries);
+        }
+    }, {
+        key: "getCacheEntries",
+        value: function getCacheEntries() {
+            var decoded = window.atob(document.cookie.replace(new RegExp("(?:(?:^|.*;\\s*)" + this.CookieName + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1"));
+            return JSON.parse(decoded || "{}");
+        }
+    }, {
+        key: "setCacheEntries",
+        value: function setCacheEntries(entries) {
+            var encoded = window.btoa(JSON.stringify(entries));
+            document.cookie = this.CookieName + "=" + encoded + ";  expires=0; path=/";
+        }
+    }]);
+    return CookieCache;
+}();
+
+var canUseLocalStorage = true;
+var fallback = null;
+try {
+    localStorage.setItem("__testing", "__testing");
+    localStorage.removeItem("__testing");
+} catch (err) {
+    canUseLocalStorage = false;
+    fallback = new MemoryCache();
+}
+var LocalStorageCache = function () {
+    function LocalStorageCache() {
+        classCallCheck(this, LocalStorageCache);
+    }
+
+    createClass(LocalStorageCache, [{
+        key: "set",
+        value: function set$$1(key, value, durationMS) {
+            if (!canUseLocalStorage) return fallback.set(key, value, durationMS);
+            var expire = durationMS ? Date.now() + durationMS : null;
+            localStorage[key] = JSON.stringify({ value: value, expire: expire });
+        }
+    }, {
+        key: "get",
+        value: function get$$1(key) {
+            if (!canUseLocalStorage) return fallback.get(key);
+            var val = JSON.parse(localStorage[key] || "null");
+            if (val) {
+                if (!isExpired(val)) {
+                    return val.value;
+                } else {
+                    this.remove(key);
+                }
+            }
+            return null;
+        }
+    }, {
+        key: "remove",
+        value: function remove(key) {
+            if (!canUseLocalStorage) return fallback.remove(key);
+            delete localStorage[key];
+        }
+    }]);
+    return LocalStorageCache;
+}();
+
+var ScopedCache = function () {
+    function ScopedCache(scope, cache) {
+        classCallCheck(this, ScopedCache);
+
+        this.scope = scope;
+        this.cache = cache;
+    }
+
+    createClass(ScopedCache, [{
+        key: "setScope",
+        value: function setScope(scope) {
+            this.scope = scope;
+        }
+    }, {
+        key: "set",
+        value: function set$$1(key, value) {
+            var durationMS = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
+            return this.cache.set(this.scope + "-" + key, value, durationMS);
+        }
+    }, {
+        key: "get",
+        value: function get$$1(key) {
+            return this.cache.get(this.scope + "-" + key);
+        }
+    }, {
+        key: "remove",
+        value: function remove(key) {
+            this.cache.remove(this.scope + "-" + key);
+        }
+    }]);
+    return ScopedCache;
+}();
+
+export { Cache, cacheFor, MemoryCache, CookieCache, LocalStorageCache, ScopedCache };
